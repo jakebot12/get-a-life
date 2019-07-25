59d98b897cb61d353a3d6c0bd78d975cda2b36c2
damonk@gmail.com <damonkd@gmail.com>
2019-07-24 19:09:55 -0700

    added categories to user model

--- models/user.js
+++ models/user.js
@@ -4,6 +4,11 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   user: { type: String, required: true },
   password: { type: String, required: true },
+  firstName: { type: String, required: false },
+  lastName: { type: String, required: false },
+  career: { type: String, required: false },
+  age: { type: String, required: false },
+  bucketItem: { type: String, required: false },
   
   date: { type: Date, default: Date.now }
 });
