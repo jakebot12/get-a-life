3c7ef01b5386c4b4360c27b86a63e81d437d61e9
Jake Gurley <jakemg12@gmail.com>
2019-07-27 19:03:57 -0700

    Created a component to load and retrieve databases from a firebase database that I also created and initialized. Still have to integrate these principles into the existing structures

---
+++ client/src/components/ImageUploads.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { storage } from "../firebase";
+// import Img from 'react-image';
+
+class ImageUploads extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+      url: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
+  }
+
+  handleChange = e => {
+    if (e.target.files[0]) {
+      const image = e.target.files[0];
+      this.setState(() => ({ image }));
+    }
+  };
+
+  handleUpload = () => {
+    const { image } = this.state;
+    console.log(image);
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    uploadTask.on(
+      "state_changed",
+      snapshot => {},
+      error => {
+        console.log(error);
+      },
+      () => {
+        storage
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then(url => {
+            console.log(url);
+            this.setState({ url });
+          });
+      }
+    );
+  };
+
+  render() {
+    const style = {
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "right",
+      justifyContent: "right"
+    };
+    return (
+      <div style={style}>
+        <input type="file" onChange={this.handleChange} />
+        <button onClick={this.handleUpload}>Upload</button>
+        <br />
+        <div className="card-img">
+        <img src={this.state.url || 'http://via.placeholder.com/200x100'}  alt="Uploaded Images" height="100" width="200"/>
+        </div>
+        {/* <img src={this.state.url || 'http://via.placeholder.com/200x100'}  alt="Uploaded Images" height="100" width="200">Image should be here</img> */}
+      </div>
+    );
+  }
+}
+
+export default ImageUploads;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,25 +1,27 @@
 import React from "react";
-var Remarkable = require('remarkable');
+import ImageUploads from "../components/ImageUploads";
+
+var Remarkable = require("remarkable");
 
 class Dashboard extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleChange = this.handleChange.bind(this);
-      this.state = { 
-        // formsControls: {
-            firstname: {value: " "},
-            lastname: {value: " "},
-            career: {value: " "},
-            age: {value: " "},
-            BucketListOne: {value: " "},
-            BucketListTwo: {value: " "},
-            BucketListThree: {value: " "}
-        }  
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      // formsControls: {
+      firstname: { value: " " },
+      lastname: { value: " " },
+      career: { value: " " },
+      age: { value: " " },
+      BucketListOne: { value: " " },
+      BucketListTwo: { value: " " },
+      BucketListThree: { value: " " }
+    };
     // }
-}
-    handleChange(e) {
-     // this.setState({ value: e.target.value });
-      console.log(e.target.value)
+  }
+  handleChange(e) {
+    // this.setState({ value: e.target.value });
+    console.log(e.target.value);
     //   formsControls: {
     //     ...this.state.formControls,[firstname]: {value: " "},
     //     lastname: {value: " "},
@@ -28,210 +30,239 @@ class Dashboard extends React.Component {
     //     BucketListOne: {value: " "},
     //     BucketListTwo: {value: " "},
     //     BucketListThree: {value: " "}
-    //     }  
+    //     }
     // });
-      };
-    
-  
-    getRawMarkup() {
-      const md = new Remarkable();
-      return { __html: md.render(this.state.value) };
-    }
-  
+  }
 
- render () {
-  return (
-    <div>
-  {/*row with settings */}
-  <div className="clr-row ">
-    <div className="clr-col-sm-12 clr-col-md-12 clr-col-lg-12">
+  getRawMarkup() {
+    const md = new Remarkable();
+    return { __html: md.render(this.state.value) };
+  }
 
-        {/*This is the navigation bar setup */}
-        {/*navbar color */}
-            <header className="header-4">
-                  <div className="branding">
-                      <a>
-                        {/*logo image/slogan*/}
-                          <img src="image/gt.jpeg" className="nav-link" alt ="Get a life logo"/>
-                          <clr-icon shape="vm-bug"></clr-icon>
-                          <span className="title"></span>
-                      </a>
-                  </div>
-                  {/*Dashboard and Browse links */}
-                  <div class="header-nav">
-                     
-                      
-                      <a href="/home" class="nav-link"><span class="nav-text">Home</span></a>
-                      <a href="/browse" class="nav-link"><span class="nav-text">Browse</span></a>
-                  </div>
-                  {/*nav bar logout */}
-                  <div className="header-actions">
-                      <a href="/welcome" className="nav-link nav-text">
-                          Log Out
-                      </a>
-                  </div>
-            </header>
-    </div>
-  </div>    
-  {/*body of page */}
-  {/*clients information card */}
-  <div className="clr-row ">
-    <div className="clr-col-lg-2 clr-col-md-4">
-        <a href="/andrewyoung" className="card clickable">
-              <div className="card-img">
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          {/*row with settings */}
+          <div className="clr-row ">
+            <div className="clr-col-sm-12 clr-col-md-12 clr-col-lg-12">
+              {/*This is the navigation bar setup */}
+              {/*navbar color */}
+              <header className="header-4">
+                <div className="branding">
+                  <a>
+                    {/*logo image/slogan*/}
+                    <img
+                      src="image/gt.jpeg"
+                      className="nav-link"
+                      alt="Get a life logo"
+                    />
+                    <clr-icon shape="vm-bug" />
+                    <span className="title" />
+                  </a>
+                </div>
+                {/*Dashboard and Browse links */}
+                <div class="header-nav">
+                  <a href="/home" class="nav-link">
+                    <span class="nav-text">Home</span>
+                  </a>
+                  <a href="/browse" class="nav-link">
+                    <span class="nav-text">Browse</span>
+                  </a>
+                </div>
+                {/*nav bar logout */}
+                <div className="header-actions">
+                  <a href="/welcome" className="nav-link nav-text">
+                    Log Out
+                  </a>
+                </div>
+              </header>
+            </div>
+          </div>
+          {/*body of page */}
+          {/*clients information card */}
+          <div className="clr-row ">
+            <div className="clr-col-lg-2 clr-col-md-4">
+              <a href="/andrewyoung" className="card clickable">
+                <div className="card-img">
                   <img src="image/andrew_young.jpeg" alt="..." />
-              </div>
-                  <div className="card-block">
-                      <p className="card-text">
-                        Name: Andrew Young
-                        <br></br>
-                        Career: Engineer
-                        <br></br>
-                        Age: 34
-                        <br></br>
-                        BucketList:
-                        <br></br>
-                        Run a marathon
-                        <br></br>
-                        Climb Everest
-                        <br></br>
-                        Marry Rhianna
-                      </p>
-                  </div>
-          </a>
-   
-  </div>
-  
- 
-  {/* second half of page */}
-  {/* colum 1 */}
-  
-    <div className="clr-col-4 clr-col-md-4" >
-        <div className="card">
-            <div className="card-block">
-                <h4 className="card-title">Bucket List Information</h4>
-                <p className="card-text">
-                    
-                        <div className="Dashboard">
-                            <form>
-                            <br/>
-                            <label>First Name: </label>   
-                            <input type="firstname"
-                                        name="firstname"
-                                        value={this.state.firstname.value}
-                                        onChange={this.handleChange}
-                                /><br/><br/>
-                            <label>Last Name: </label>  
-                            <input type="lastname"
-                                    name="lastname"
-                                    value={this.state.lastname.value}
-                                    onChange={this.handleChange}
-                            /> <br/><br/>
-                            <label>Career: </label>  
-                            <input type="career"
-                                    name="career"
-                                    value={this.state.career.value}
-                                    onChange={this.handleChange}
-                            /> <br/><br/>
-                            <label> Age: </label>  
-                            <input type="age"
-                                    name="age"
-                                    value={this.state.age.value}
-                                    onChange={this.handleChange}
-                            /> <br/><br/>
-                            <label>Bucket List One : </label>  
-                            <input type="BucketListOne"
-                                    name="BucketListOne"
-                                    value={this.state.BucketListOne.value}
-                                    onChange={this.handleChange}
-                            /> <br/><br/>
-                             <label>Bucket List Two : </label>  
-                            <input type="BucketListTwo"
-                                    name="BucketListTwo"
-                                    value={this.state.BucketListTwo.value}
-                                    onChange={this.handleChange}
-                            /> <br/><br/>
-                             <label>Bucket List Three : </label>  
-                            <input type="BucketListThree"
-                                    name="BucketListThree"
-                                    value={this.state.BucketListThree.value}
-                                    onChange={this.handleChange}
-                            /> <br/><br/>
+                </div>
+                <div className="card-block">
+                  <p className="card-text">
+                    Name: Andrew Young
+                    <br />
+                    Career: Engineer
+                    <br />
+                    Age: 34
+                    <br />
+                    BucketList:
+                    <br />
+                    Run a marathon
+                    <br />
+                    Climb Everest
+                    <br />
+                    Marry Rhianna
+                  </p>
+                </div>
+              </a>
+            </div>
 
-                            </form>
-                            {/* <h3>Input</h3>
+            {/* second half of page */}
+            {/* colum 1 */}
+
+            <div className="clr-col-4 clr-col-md-4">
+              <div className="card">
+                <div className="card-block">
+                  <h4 className="card-title">Bucket List Information</h4>
+                  <p className="card-text">
+                    <div className="Dashboard">
+                      <form>
+                        <br />
+                        <label>First Name: </label>
+                        <input
+                          type="firstname"
+                          name="firstname"
+                          value={this.state.firstname.value}
+                          onChange={this.handleChange}
+                        />
+                        <br />
+                        <br />
+                        <label>Last Name: </label>
+                        <input
+                          type="lastname"
+                          name="lastname"
+                          value={this.state.lastname.value}
+                          onChange={this.handleChange}
+                        />{" "}
+                        <br />
+                        <br />
+                        <label>Career: </label>
+                        <input
+                          type="career"
+                          name="career"
+                          value={this.state.career.value}
+                          onChange={this.handleChange}
+                        />{" "}
+                        <br />
+                        <br />
+                        <label> Age: </label>
+                        <input
+                          type="age"
+                          name="age"
+                          value={this.state.age.value}
+                          onChange={this.handleChange}
+                        />{" "}
+                        <br />
+                        <br />
+                        <label>Bucket List One : </label>
+                        <input
+                          type="BucketListOne"
+                          name="BucketListOne"
+                          value={this.state.BucketListOne.value}
+                          onChange={this.handleChange}
+                        />{" "}
+                        <br />
+                        <br />
+                        <label>Bucket List Two : </label>
+                        <input
+                          type="BucketListTwo"
+                          name="BucketListTwo"
+                          value={this.state.BucketListTwo.value}
+                          onChange={this.handleChange}
+                        />{" "}
+                        <br />
+                        <br />
+                        <label>Bucket List Three : </label>
+                        <input
+                          type="BucketListThree"
+                          name="BucketListThree"
+                          value={this.state.BucketListThree.value}
+                          onChange={this.handleChange}
+                        />{" "}
+                        <br />
+                        <br />
+                      </form>
+                      {/* <h3>Input</h3>
                             <label htmlFor="markdown-content">
 
                             </label> */}
-                            {/* <textarea
+                      {/* <textarea
                             id="markdown-content"
                             onChange={this.handleChange}
                             defaultValue={this.state.value}
                             /> */}
-                             {/* This is where the text output happens*/}
-                            {/* <h3>Output</h3> */}
-                            <div
-                            className="content"
-                            dangerouslySetInnerHTML={this.getRawMarkup()}
-                            />
-                   
-                    </div>      
-                </p>
-            </div>
-                    <div className="card-footer">
-                        <div className="progress">
-                            <progress value="..." max="100"></progress>
-                        </div>
-                            <a href="..." className="card-link">Update</a>
+                      {/* This is where the text output happens*/}
+                      {/* <h3>Output</h3> */}
+                      <div
+                        className="content"
+                        dangerouslySetInnerHTML={this.getRawMarkup()}
+                      />
                     </div>
-        </div>
-        <div className="card">
-            <div className="card-block">
-                <h4 className="card-title">Upload Photo</h4>
-                <p className="card-text">
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <div className="progress">
+                    <progress value="..." max="100" />
+                  </div>
+                  <a href="..." className="card-link">
+                    Update
+                  </a>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-block">
+                  <h4 className="card-title">Upload Photo</h4>
+                  <p className="card-text">
                     <div>
-                        <form className="clr-form clr-form-compact">
-                            <div className="clr-form-control clr-row">
-                                <label for="example" className="clr-control-label clr-col-12 clr-col-md-2">Add Photo Address</label>
-                                <div className="clr-control-container clr-col-12 clr-col-md-10">
-                                    <div className="clr-input-wrapper">
-                                        <input type="text" id="example" placeholder="Example Input" className="clr-input"/>
-                                        <clr-icon className="clr-validate-icon" shape="exclamation-circle"> </clr-icon>
-                                    </div>
-                                    <span className="clr-subtext">Are you ready!</span>
-                                </div>
+                      <form className="clr-form clr-form-compact">
+                        <div className="clr-form-control clr-row">
+                          <label
+                            for="example"
+                            className="clr-control-label clr-col-12 clr-col-md-2"
+                          >
+                            Add Photo Address
+                          </label>
+                          <div className="clr-control-container clr-col-12 clr-col-md-10">
+                            <div className="clr-input-wrapper">
+                              <input
+                                type="text"
+                                id="example"
+                                placeholder="Example Input"
+                                className="clr-input"
+                              />
+                              <clr-icon
+                                className="clr-validate-icon"
+                                shape="exclamation-circle"
+                              >
+                                {" "}
+                              </clr-icon>
                             </div>
-                        </form>
-                    </div>      
-                </p>
-            </div>
-                    <div className="card-footer">
-                        <div className="progress">
-                            <progress value="..." max="100"></progress>
+                            <span className="clr-subtext">Are you ready!</span>
+                          </div>
                         </div>
-                            <a href="..." className="card-link">Add</a>
+                      </form>
                     </div>
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <div className="progress">
+                    <progress value="..." max="100" />
+                  </div>
+                  <a href="..." className="card-link">
+                    Add
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* colum 2 */}
+            <ImageUploads/>
+          </div>
         </div>
-    </div> 
-       
-    
-    
-    {/* colum 2 */}
- 
-      
-   
-      
-    
-    
-    
-    </div>
-
-</div>
-
-
-
-  );
-}
+        
+      </React.Fragment>
+    );
+  }
 }
 
 export default Dashboard;
