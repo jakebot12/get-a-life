23147e9be40820e3127a6586b401cd26e11468ec
Jake Gurley <jakemg12@gmail.com>
2019-07-29 19:09:25 -0700

    Repaired firebase files that were destroyed in the repo madness. Hopefully we can get it pushed in time for the Finals presentation in a minute.lol

--- client/src/pages/NavTabs.js
+++ client/src/pages/NavTabs.js
@@ -1,45 +1,61 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 function NavTabs() {
   return (
-    <div>
-      <div className="clr-row ">
-        {/*Navbar Row*/}
-        <div className="clr-col-sm-12 clr-col-md-12 clr-col-lg-12">
-
-
-          {/*navbar color */}
-          <header className="header-4">
-            <div className="branding">
-              {/*logo image/slogan*/}
-              <a>
-                <img src="image/gt.jpeg" className="nav-link" alt="Get a life logo" />
-                <clr-icon shape="vm-bug"></clr-icon>
-                <span className="title"></span>
-              </a>
-            </div>
-            {/*Dashboard and Browse links */}
-            <div className="header-nav">
-
-
-              <a href="/home" className="nav-link"><span className="nav-text">Home</span></a>
-              <a href="/browse" className="nav-link"><span className="nav-text">Browse</span></a>
-              <a href="/dashboard" className="nav-link"><span className="nav-text">Dashboard</span></a>
-              <a href="/contact" className="nav-link"><span className="nav-text">Contact</span></a>
-            </div>
-            {/*nav bar logout */}
-            <div className="header-actions">
-              <a href="/welcome" className="nav-link nav-text">
-                Log Out
-                        </a>
-            </div>
-          </header>
-        </div>
-      </div>
-
-
-    </div>
+    <ul className="nav nav-tabs">
+    <li className="nav-item">
+        <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          
+        </Link>
+      </li>
+    
+      <li className="nav-item">
+        <Link to="/home" className={window.location.pathname === "/home" ? "nav-link active" : "nav-link"}>
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/dashboard"
+          className={window.location.pathname === "/dashboard" ? "nav-link active" : "nav-link"}
+        >
+          Dashboard
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/browse"
+          className={window.location.pathname === "/browse" ? "nav-link active" : "nav-link"}
+        >
+          Browse
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/contact"
+          className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/login"
+          className={window.location.pathname === "/login" ? "nav-link active" : "nav-link"}
+        >
+         Login
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/signup"
+          className={window.location.pathname === "/signup" ? "nav-link active" : "nav-link"}
+        >
+         Signup
+        </Link>
+      </li>
+    </ul>
   );
 }
 
